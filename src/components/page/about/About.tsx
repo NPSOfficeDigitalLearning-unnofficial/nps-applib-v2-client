af569428f23f4c8910c42f7f85db146a22bbf824
SciDev5 <67175934+SciDev5@users.noreply.github.com>
2021-08-12 19:59:42 -0400

    Implement React component for about page.

--- src/components/page/about/About.tsx
+++ src/components/page/about/About.tsx
@@ -1,10 +1,21 @@
 import React, { ReactNode } from "react";
+import { Trans } from "react-i18next";
+import WidthLimiter from "../../leaf-component/WidthLimiter/WidthLimiter";
 import "./About.scss";
 
 export default class AboutPage extends React.Component {
     render():ReactNode {
-        return (<>
-            <main>TODO: about page</main>
-        </>);
+        return (<main className="AboutPage">
+            <WidthLimiter>
+                <h1><Trans>page.about.name</Trans></h1>
+                <p>
+                    <Trans>page.about.paragraph1</Trans>
+                </p>
+                <p>
+                    <Trans>page.about.paragraph2</Trans>
+                    <a href="https://github.com/SciDev5/nps-applib-v2-cli">https://github.com/SciDev5/nps-applib-v2-cli</a>.
+                </p>
+            </WidthLimiter>
+        </main>);
     }
 }
